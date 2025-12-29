eb08b44607cd8d9beb9e07fa72232b9b38d5c2d9
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2025-12-29 07:49:11 +0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export  default function Layout({ children }) {
+    return (
+        <html lang="en">
+            <head>
+                <title>Learn Next.js Fundamental</title>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
