f32e89fe823356c991572145d8981e24644cd52c
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2026-01-01 12:27:56 +0700

    update markdown

--- app/blog/belajar-nextjs/page.tsx
+++ app/blog/belajar-nextjs/page.tsx
@@ -2,17 +2,24 @@ import Heading from "@/components/Heading";
 import { readFile } from "node:fs/promises";
 import { marked } from "marked";
 import { join } from "node:path";
+import matter from "gray-matter";
 
 export default async function Home() {
     const filePath = join(process.cwd(), 'content', 'blog' , 'belajar-nextjs.md');
     const text = await readFile(filePath, 'utf-8');
 
-    const html = marked(text);
+    const { content, data: { title, image, date, authors } }  = matter(text);
+
+    const html = marked(content);
+
+
+    // console.log(data);
 
     return (
         <>
-            <Heading title="Belajar Next JS" />
-            <img src="/images/gedung.png"       className="rounded-lg shadow-md mb-5"
+            <Heading title={title} />
+            <p className="italic text-sm pb-2">{date} - {authors}</p>
+            <img src={image}      className="rounded-lg shadow-md mb-5"
                 width={600} height={400} />
             <article 
                 dangerouslySetInnerHTML={{ __html: html }} 
--- content/blog/belajar-nextjs.md
+++ content/blog/belajar-nextjs.md
@@ -1,12 +1,16 @@
-# Heading 
+---
+title : "Belajar next.js"
+image : "/images/gedung.png"
+date : "26/11/2023"
+authors : "admin"
 
-## Sub-heading
+---
 
-Rich text with **bold** and _italic_.
 
-New Paragraph
+# What is Next.js?
 
-List:
-* satu
-* dua
-* tiga
+Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.
+
+It also automatically configures lower-level tools like bundlers and compilers. You can instead focus on building your product and shipping quickly.
+
+Whether you're an individual developer or part of a larger team, Next.js can help you build interactive, dynamic, and fast React applications.
